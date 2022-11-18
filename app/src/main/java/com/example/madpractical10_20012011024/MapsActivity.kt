8590859d473fdf5a66e63ea8c79d2475f649d949
Niraj-Chothani <nirajchothani20@gnu.ac.in>
2022-11-18 08:40:13 +0530

    push

--- app/src/main/java/com/example/madpractical10_20012011024/MapsActivity.kt
+++ app/src/main/java/com/example/madpractical10_20012011024/MapsActivity.kt
@@ -1,11 +1,10 @@
 package com.example.madpractical10_20012011024
 
-import android.app.Person
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import com.example.madpractical10_20012011024.databinding.ActivityMapsBinding
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -16,31 +15,31 @@ import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private val TAG = "MapActivity"
-    private lateinit var mMap: GoogleMap
-    private lateinit var binding: ActivityMapsBinding
-    private var lat = -34.0
-    private var log = 151.0
-    private var title = "Marker in Sydney"
+        private val TAG = "MapActivity"
+        private lateinit var mMap: GoogleMap
+        private lateinit var binding: ActivityMapsBinding
+        private var lat = -34.0
+        private var log = 151.0
+        private var title = "Marker in Sydney"
 
-    @RequiresApi(Build.VERSION_CODES.P)
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val obj = intent.getSerializableExtra("Object") as Person
-        Log.i(TAG, "onCreate: Object:$obj")
-//        lat = obj.Latitude
-//        log = obj.Longitude
-//        title = obj.Name
-        binding = ActivityMapsBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        @RequiresApi(Build.VERSION_CODES.P)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            val obj = intent.getSerializableExtra("Object") as Person
+            Log.i(TAG, "onCreate: Object:$obj")
+            lat = obj.Latitude
+            log = obj.Longitude
+            title = obj.Name
+            binding = ActivityMapsBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+            val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+            mapFragment.getMapAsync(this)
+        }
+        override fun onMapReady(googleMap: GoogleMap) {
+            mMap = googleMap
+            val sydney = LatLng(lat, log)
+            mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        }
     }
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-        val sydney = LatLng(lat, log)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-    }
-}
